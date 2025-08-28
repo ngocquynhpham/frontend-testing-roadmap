26b2025d441875209e51df5c118fb9c759f78b25
ngocquynhph <quynh.pham@sungrove.co.jp>
2025-08-28 11:51:17 +0700

    chore: scaffold React + Jest + RTL

---
+++ jest/src/components/Button.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Button from './Button';
+
+test('renders and clicks', () => {
+  const fn = jest.fn(); 
+  render(<Button onClick={fn}>Click me</Button>);
+  fireEvent.click(screen.getByText('Click me'));
+  expect(fn).toHaveBeenCalledTimes(1);
+});
